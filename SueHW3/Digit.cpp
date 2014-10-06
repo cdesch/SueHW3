7db82f191f9029a0995ac6b661f5a15ed2f05ac0
Chris <cdesch@gmail.com>
2014-10-06 15:27:33 -0400

    Adding Digit Class
    Signed-off-by: Chris <cdesch@gmail.com>

---
+++ SueHW3/Digit.cpp
@@ -0,0 +1,9 @@
+//
+//  Digit.cpp
+//  SueHW3
+//
+//  Created by cj on 10/6/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#include "Digit.h"
---
+++ SueHW3/Digit.h
@@ -0,0 +1,14 @@
+//
+//  Digit.h
+//  SueHW3
+//
+//  Created by cj on 10/6/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#ifndef __SueHW3__Digit__
+#define __SueHW3__Digit__
+
+#include <stdio.h>
+
+#endif /* defined(__SueHW3__Digit__) */
